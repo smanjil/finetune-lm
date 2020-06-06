16bcacb5688befbaa99e30ae64a2813921adf063
smanjil <shresthamanjil21@gmail.com>
2020-06-06 10:57:16 +0200

    correcting

--- ft-freeze.py
+++ ft-freeze.py
@@ -947,7 +947,7 @@ def main():
     #### This section works only with AutoModelWithLMHead
     for param in model.named_parameters():
         if 'bert.encoder.layer' in param[0]:
-            if '8' not in param[0] or '9' not in param[0], '10' not in param[0], '11' not in param[0]:
+            if '8' not in param[0] or '9' not in param[0] or '10' not in param[0] or '11' not in param[0]:
                 param[1].requires_grad = False
         print(param[0], param[1].requires_grad)
     ######### freeze end
